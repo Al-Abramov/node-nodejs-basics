6a8ca585f47317f45ff98badfc00f4f17bb02593
Alexey Abramov <aal@renue.ru>
2024-10-08 17:01:08 +0400

    fix

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -20,5 +20,4 @@ const spawnChildProcess = async (args) => {
     });
 };
 
-// Put your arguments in function call to test this functionality
 spawnChildProcess(['someArgument1', 'someArgument2']);
